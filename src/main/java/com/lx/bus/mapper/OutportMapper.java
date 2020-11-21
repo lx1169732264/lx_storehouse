0d93c826d00ed492116a6313b71febd3f988f27f
lx <1169732264@qq.com>
2020-11-21 18:18:25 +0800

    20.11.21

--- src/main/java/com/lx/bus/mapper/OutportMapper.java
+++ src/main/java/com/lx/bus/mapper/OutportMapper.java
@@ -2,7 +2,8 @@ package com.lx.bus.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lx.bus.domain.Outport;
+import org.apache.ibatis.annotations.Param;
 
 public interface OutportMapper extends BaseMapper<Outport> {
-    Integer queryOutPortSum(Long inportid,int goodsid);
+    Integer queryOutPortSum(@Param("inportid") Long inportid, @Param("goodsid") int goodsid);
 }
